d927d221f7d1515da0482fbfcc21746e824a000f
Jozef Rozsa <jrozsa@frinx.io>
2018-07-30 17:41:03 +0200

    Improved healthcheck at startup
    Stops containers if one not healthy
    
    Signed-off-by: Jozef Rozsa <jrozsa@frinx.io>

--- health_check.sh
+++ health_check.sh
@@ -2,16 +2,32 @@
 
 echo 'Health check:'
 
+declare -i result=0
+
 ### Check ODl
 ./wait_for_it.sh localhost:8181
+result+=$?
+
 ### Check Elastisearch 
 #curl -X GET 'http://localhost:9200/_cluster/health'
 ./wait_for_it.sh localhost:9200
+result+=$?
+
 ### Check Microservices
 ./wait_for_it.sh localhost:6000
+result+=$?
+
 ### Check Coductor-server
 ./wait_for_it.sh localhost:8080
+result+=$?
+
 ### Check conductor-ui
 ./wait_for_it.sh localhost:5000
+result+=$?
+
+
 ### Check dynomite
 #curl -X GET 'http://host:22222/ping'
+exit $result
+
+
--- startup.sh
+++ startup.sh
@@ -13,6 +13,16 @@ sleep 30
 
 ### Health check
 ./health_check.sh
+if [[ $? -ne 0 ]]
+then
+  RED='\033[0;31m'
+  NC='\033[0m' # No Color
+  printf "${RED}System is not healthy. Shutting down.\n${NC}"
+  echo "Removing containers."
+  ./teardown.sh
+  exit
+fi
+
 
 
 ### Import Frinx Tasks and Workflow defs
