d5163ddce76fb2fa3969f4f275e30d519bff3905
Jozef Rozsa <jrozsa@frinx.io>
2019-03-06 12:33:23 +0100

    Small refactor
    Remove 00 from start
    Add sudo to workflow import command
    
    Signed-off-by: Jozef Rozsa <jrozsa@frinx.io>

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-00
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
@@ -60,7 +60,7 @@ done
 
 function import_workflows {
 
-docker exec micros bash -c "cd /home/app && newman run netinfra_utils/postman.json --folder 'SETUP' -e netinfra_utils/postman_environment.json"
+sudo docker exec micros bash -c "cd /home/app && newman run netinfra_utils/postman.json --folder 'SETUP' -e netinfra_utils/postman_environment.json"
 if [ "$skip" = false ]; then
   check_success $?
 fi
