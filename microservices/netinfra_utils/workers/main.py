1929e34df8f5c8fbbbb7f1c3c3682b0e1dba99ea
Maros Marsalek <mmarsalek@frinx.io>
2019-07-16 10:24:49 +0200

    Add standalone push style microservice instance
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

--- microservices/netinfra_utils/workers/main.py
+++ microservices/netinfra_utils/workers/main.py
@@ -1,13 +1,13 @@
 import time
-import worker_wrapper
 from frinx_rest import conductor_url_base
+import worker_wrapper
 import standalone_main
 
 
 def main():
     print('Starting FRINX workers')
     cc = worker_wrapper.WorkerWrapper(conductor_url_base, 1, 0.1)
-    worker_wrapper.register_workers(cc)
+    standalone_main.register_workers(cc)
 
     # block
     while 1:
