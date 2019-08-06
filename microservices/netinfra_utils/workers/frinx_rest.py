8dbf210cbc33d2b5da6e48d7c10cf420624ce85f
Jozef Rózsa <jrozsa@frinx.io>
2019-08-06 19:12:25 +0200

    Comment custom auth part
    Signed-off-by: Jozef Rózsa <jrozsa@frinx.io>

--- microservices/netinfra_utils/workers/frinx_rest.py
+++ microservices/netinfra_utils/workers/frinx_rest.py
@@ -4,16 +4,16 @@ odl_url_base = "http://odl:8181/restconf"
 elastic_url_base = "http://elasticsearch:9200"
 conductor_url_base = "http://conductor-server:8080/api"
 
-username = ''
-password = ''
+# username = ''
+# password = ''
+#
+# with open('/opt/odl_pass/db-credentials.txt', 'r') as f:
+#     lines = f.readlines()
+#     username = lines[0].split(':')[1].rstrip('\n')
+#     password = lines[1].split(':')[1].rstrip('\n')
 
-with open('/opt/odl_pass/db-credentials.txt', 'r') as f:
-    lines = f.readlines()
-    username = lines[0].split(':')[1].rstrip('\n')
-    password = lines[1].split(':')[1].rstrip('\n')
-
-#odl_credentials = ('admin', 'admin')
-odl_credentials = (username, password)
+odl_credentials = ('admin', 'admin')
+#odl_credentials = (username, password)
 odl_headers = {"Content-Type": "application/json"}
 elastic_headers = {"Content-Type": "application/json"}
 
