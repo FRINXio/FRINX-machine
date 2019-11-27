b488038cd0da4eba987acee36bd9714666d8f250
Tomas <thodor@frinx.io>
2019-11-27 16:02:47 +0100

    Added workflow for netconf-testtol configuration and adjust L2VPN wf
    Signed-off-by: Tomas <thodor@frinx.io>

--- startup.sh
+++ startup.sh
@@ -115,6 +115,8 @@ import_workflows
 # Import devices
 import_devices
 
+curl --silent -H "Content-Type: application/json"  -X POST -d "{\"name\":\"Write_data_to_netconf_testool\",\"version\":1,\"input\":{}}" http://localhost:8080/api/workflow/ &>/dev/null
+
 echo -e 'Startup finished!\n'
 echo -e 'FRINX UniConfig UI is ready and listening on port 3000. e.g. http://localhost:3000/\n'
 
