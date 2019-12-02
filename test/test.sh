4582404eb630e134663ae17b1406c8c2b8a91d43
Jozef Rózsa <jrozsa@frinx.io>
2019-12-02 11:06:23 +0100

    Update test
    Signed-off-by: Jozef Rózsa <jrozsa@frinx.io>

--- test/test.sh
+++ test/test.sh
@@ -11,7 +11,6 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
 # Ger container ips
-odl_ip=$(docker inspect -f "{{with index .NetworkSettings.Networks \"frinx-machine_default\"}}{{.IPAddress}}{{end}}"  odl)
 conductor_ip=$(docker inspect -f "{{with index .NetworkSettings.Networks \"frinx-machine_default\"}}{{.IPAddress}}{{end}}"  conductor-server)
 sample_topology_ip=$(docker inspect -f "{{with index .NetworkSettings.Networks \"frinx-machine_default\"}}{{.IPAddress}}{{end}}"  sample-topology)
 
@@ -20,7 +19,6 @@ cp ${DIR}/FRINX-machine_test_env.postman_environment.json FRINX-machine_test_env
 
 # Modiy tmp env file, with actual container ips
 sed -i -- "s/var_conductor_ip/${conductor_ip}/g" FRINX-machine_test_env.postman_environment_tmp.json
-sed -i -- "s/var_odl_ip/${odl_ip}/g" FRINX-machine_test_env.postman_environment_tmp.json
 sed -i -- "s/var_sample_topology_ip/${sample_topology_ip}/g" FRINX-machine_test_env.postman_environment_tmp.json
 
 newman run ./FRINX-machine_test.postman_collection.json -e ./FRINX-machine_test_env.postman_environment_tmp.json
