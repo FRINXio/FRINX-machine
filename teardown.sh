cd5b03345d1a97d87d05284c540d008b72f34a0a
Ladislav Moravek <lmoravek@frinx.io>
2020-02-18 10:50:29 +0100

    Added options to remove FM volumes and images
    Issue-ID: FM-208
    Signed-off-by: Ladislav Moravek <lmoravek@frinx.io>

--- teardown.sh
+++ teardown.sh
@@ -3,6 +3,47 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
+script="teardown.sh"
 
-### Stop and remove containers
-docker-compose down
+# Common functions
+function usage {
+    echo -e "Usage: $script [-v|--volumes] [-i|--images]\n"
+}
+
+function help {
+    usage
+    echo -e "OPTIONS:"
+    echo -e "  -v | --volumes             Removes volumes specified in the docker-compose.yml file"
+    echo -e "  -i | --images              Removes all images used by any service specified in the docker-compose.yml file"
+    echo -e "\n"
+}
+
+
+VOLUMES=""
+IMAGES=""
+
+# Args while-loop
+while [ "$1" != "" ];
+do
+case $1 in
+   -v | --volumes )
+   VOLUMES="--volumes"
+   ;;
+   -i | --images )
+   IMAGES="--rmi all"
+   ;;
+   -h | --help )
+   help
+   exit
+   ;;
+   *)
+    echo "Unknown option: '$1'"
+    help
+    exit
+   ;;
+esac
+shift
+done
+
+### Stop and remove containers, optionally also volumes and images
+docker-compose down $VOLUMES $IMAGES --remove-orphans
