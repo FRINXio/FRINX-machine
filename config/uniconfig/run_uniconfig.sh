dee8a87d81e93ad9c5b1686d44502cc600a27d1e
Jozef Volak <jvolak@frinx.io>
2021-11-15 10:57:08 +0100

    Separate logging files for multiinstance Uniconfig-controller

---
+++ config/uniconfig/run_uniconfig.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+CONFIG="config/lighty-uniconfig-config.json"
+JAR_DIRS="./*:./libs/*:./config"
+MAIN_CLASS="io.frinx.lighty_uniconfig.Main"
+JAVA_MAX_MEM=${JAVA_MAX_MEM:="4G"}
+DEBUG_PARAMETER="--debug"
+UNICONFIG_ID=${CONTAINER_ID:=1}
+
+display_usage() {
+    echo -e "Usage: $(basename "$0") [-f] [-l LICENSE_TOKEN] [--debug]"
+    echo -e "where: "
+    echo -e "   -l LICENSE_TOKEN : license token for running Frinx Uniconfig"
+    echo -e "   -f               : new license token is forced (overwrites old license)"
+    echo -e "   --debug          : enabled java debugging on port 5005"
+}
+
+is_enabled_debugging() {
+  for arg in "$@"; do
+    if [ "$arg" = $DEBUG_PARAMETER ]; then
+      return 1
+    fi
+  done
+  return 0
+}
+
+delete_debug_argument() {
+  filtered_args=("$@")
+  for i in "${!filtered_args[@]}"; do
+    if [ "${filtered_args[i]}" = "$DEBUG_PARAMETER" ]; then
+      unset 'filtered_args[i]'
+    fi
+  done
+}
+
+for i in "$@"
+do
+case $i in
+    -h|--help)
+    display_usage
+    exit 0
+    ;;
+esac
+done
+
+# removing cached data and logs from previous run
+rm -rf snapshots/ journal/ log/${UNICONFIG_ID}/* log/gc-${UNICONFIG_ID}.lo*
+
+# folder where lighty stores data
+mkdir -m 700 -p data
+
+is_enabled_debugging "$@"; enabled_debugging=$?
+if [ $enabled_debugging -eq 1 ]; then
+  delete_debug_argument "$@"
+  java "-Xmx${JAVA_MAX_MEM}" -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -cp "${JAR_DIRS}" \
+    "${MAIN_CLASS}" -c "${CONFIG}" "${filtered_args[@]}"; unset filtered_args
+else
+  java "-Xmx${JAVA_MAX_MEM}" -cp "${JAR_DIRS}" "${MAIN_CLASS}" -c "${CONFIG}" "$@"
+fi
+unset enabled_debugging
