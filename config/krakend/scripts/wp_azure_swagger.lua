bda6c88ccc9c2d091714c18aa33ed1222b80207b
Jozef Volak <jvolak@frinx.io>
2022-07-04 18:02:04 +0200

    Swagger auth compatibility

--- config/krakend/scripts/wp_azure_swagger.lua
+++ config/krakend/scripts/wp_azure_swagger.lua
@@ -4,11 +4,11 @@ function swagger_auth_modal(r,jwt,scope,client_id)
         -- change token request to id_token
         body = body:gsub("response_type=token", "response_type=id_token");
         -- -- -- disable client_id input box and use default
-        body = body:gsub("||A===x", "");
+        body = body:gsub("||C===x", "");
         body = body:gsub('client_id="%+encodeURIComponent%(p%)', 'client_id=' .. client_id .. '"');
         -- -- -- -- disable scopes selection and use default, add nonce query parameter
-        body = body:gsub('O=o.get%("allowedScopes"%)||o.get%("scopes"%),', "");
-        body = body:gsub('T%(%)%(c%)%?m%=c:F.a.List.isList%(c%)&&%(m=c.toArray%(%)%),m.length%>0', "true");
+        body = body:gsub('C,j=o.get%("allowedScopes"%)||o.get%("scopes"%),', "");
+        body = body:gsub('T%(%)%(l%)%?m%=l:F%(%).List.isList%(l%)&&%(m=l.toArray%(%)%),m.length%>0', "true");
         body = body:gsub('scope="%+encodeURIComponent%(m.join%(v%)%)', 'scope=' .. scope .. '&nonce=678910"');
         r:headers('Content-Length', tostring(body:len()))
     end
