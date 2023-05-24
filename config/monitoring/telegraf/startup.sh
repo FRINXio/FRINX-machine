80f9331b4fc986c19cd1e30ab4093b7376871d55
Jozef Volak <jvolak@frinx.io>
2023-05-24 12:03:49 +0200

    Fix permission issue in telegaf service Edit config file in tmp folder Increate CPU/Memory limits

--- config/monitoring/telegraf/startup.sh
+++ config/monitoring/telegraf/startup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+CONFIG_FILE="/etc/telegraf/telegraf.conf"
+TMP_CONFIG_FILE="/tmp/telegraf.conf"
+
+echo "Moving ${CONFIG_FILE} to ${TMP_CONFIG_FILE}"
+cp ${CONFIG_FILE} ${TMP_CONFIG_FILE}
+
 # set sensitive env variables from docker secrets
 if [[ -f "/set_env_secrets.sh" ]]; then
   . /set_env_secrets.sh ''
@@ -7,7 +13,9 @@ fi
 
 # remove manager configuration for non manager instances
 if [[ "${HOSTNAME}" != "${MANAGER_HOSTNAME}" ]]; then
-    sed -i '/# MANAGER/,$d'  telegraf.conf
+    echo "Remove manager configuration from ${TMP_CONFIG_FILE}"
+    sed -i '/# MANAGER/,$d'  ${TMP_CONFIG_FILE}
 fi
 
-telegraf "$@"
+cat ${TMP_CONFIG_FILE}
+telegraf --config ${TMP_CONFIG_FILE} "$@"
